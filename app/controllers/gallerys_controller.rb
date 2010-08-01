3096f1a7228d28c3f0300ab7584474be74949a76
Marc Lagrange <rhaamo@gruik.at>
2010-08-01 19:36:25 +0200

    Add default layout for gallerys and some things

--- app/controllers/gallerys_controller.rb
+++ app/controllers/gallerys_controller.rb
@@ -1,5 +1,16 @@
 class GallerysController < ApplicationController
+  layout :set_layout
   
   def index
+  end
+  
+  private
+  def set_layout
+    if action_name == "index"
+      return "gallerys_layout"
+    end
+    if action_name == "show"
+      return "album_layout"
+    end
   end
 end
