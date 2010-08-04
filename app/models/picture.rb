f7659cf595c6897e83a1972c3581daaf56368e8e
Marc Lagrange <rhaamo@gruik.at>
2010-08-04 21:45:43 +0200

    Add basic carrierwave stuff, remove paperclip stuff too

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,22 +4,12 @@ class Picture < ActiveRecord::Base
   has_one :metadata
   has_one :exif
 
-  has_attached_file :photo,
-    :styles => { 
-      :large => "1024x",
-      :medium => "500x",
-      :normal => "640x",
-      :small => "240x",
-      :thumb => "100x"
-    },
-    :default_style => :normal
-    
-    process_in_background :photo
-    
-    validates_attachment_content_type :photo, :content_type => [
-      "image/jpeg", "image/png", "image/gif"
-    ]
-    validates_attachment_presence :photo
+    #:styles => { 
+    #  :large => "1024x",
+    #  :medium => "500x",
+    #  :normal => "640x",
+    #  :small => "240x",
+    #  :thumb => "100x"    
 
     after_create :process_metadatas_exif
     scope :processed, lambda { { :conditions => ['photo_processing = ?', false] } }
