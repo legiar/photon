fe7f436a1dcfc25ecd64f7cf8a7eb1bdfc6d2d81
Marc Lagrange <rhaamo@gruik.at>
2010-08-05 23:53:57 +0200

    Add basic pagination system, probably buggy

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -7,4 +7,27 @@ module PicturesHelper
       return File.join("/", store_dir, "default", "invalid.png")
     end
   end
+  
+  def navigation_objects(album, photo)
+    # Return three objects
+    index_in_album = album.pictures.index(photo)
+    album_size = album.pictures.size
+    pictures = album.pictures
+    
+    case index_in_album
+      when 0
+        # first
+        # Return actual act+1 act+2
+        return [photo, pictures[1], pictures[2]]
+      when album_size
+        # last
+        # return act act-1 act-2
+        return [pictures[album_size-2], pictures[album_size-1], photo]
+      else
+        # not first and not last :D
+        # return act-1 act act+1
+        return [pictures[index_in_album-1], photo, pictures[index_in_album+1]]
+    end
+  end
+    
 end
