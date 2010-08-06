01d0b0339bb383560cc4c5a6ce3c53d61b326e0c
Marc Lagrange <rhaamo@gruik.at>
2010-08-06 10:01:43 +0200

    Add support for GPS Map

---
+++ public/javascripts/map.js
@@ -0,0 +1,24 @@
+var map, layer;
+var proj_4326 = new OpenLayers.Projection("EPSG:4326");
+var proj_900913 = new OpenLayers.Projection("EPSG:900913");
+
+function initOpenLayerMap(lon, lat, div) {
+	origin = new OpenLayers.Geometry.Point(lon, lat);
+	
+	map = new OpenLayers.Map(div, {displayProjection: proj_4326, controls: []});
+	layer = new OpenLayers.Layer.OSM();
+	
+	var markers = new OpenLayers.Layer.Markers( "Markers" );
+	var size = new OpenLayers.Size(20,27);
+	var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+	var icon = new OpenLayers.Icon('/images/marker.png', size, offset);
+	
+	marker = new OpenLayers.Marker(new OpenLayers.LonLat(lon, lat).transform(proj_4326, proj_900913), icon);
+	markers.addMarker( marker );
+	
+	map.addLayer(layer);
+	map.addLayer(markers);
+	map.setCenter( new OpenLayers.LonLat(lon, lat).transform(proj_4326, proj_900913), 15);
+
+
+}
